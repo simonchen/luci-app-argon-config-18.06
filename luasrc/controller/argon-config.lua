3d1e01b0fecb75f8f75146763b6c64fb946439f8
simonchen <chenxinyi1978@hotmail.com>
2026-03-10 22:15:49 +0800

    New entry: check_sysinfo

--- luasrc/controller/argon-config.lua
+++ luasrc/controller/argon-config.lua
@@ -5,6 +5,21 @@ function index()
 		return
 	end
 
+        entry({"check_sysinfo"}, call("action_check_sysinfo"), nil).sysauth = false
+
 	local page = entry({"admin", "system", "argon_config"}, form("argon-config"), _("Argon Config"), 90)
 	page.acl_depends = { "luci-app-argon-config" }
 end
+
+function action_check_sysinfo()
+        local sysinfo = luci.util.ubus("system", "info") or { }
+
+        luci.http.status(200, "OK")
+	luci.http.header("Cache-Control", "no-cache, must-revalidate")
+	luci.http.header("Access-Control-Allow-Origin", "*")
+        luci.http.prepare_content("application/json")
+        luci.http.write_json(sysinfo)
+
+	return
+end
+
